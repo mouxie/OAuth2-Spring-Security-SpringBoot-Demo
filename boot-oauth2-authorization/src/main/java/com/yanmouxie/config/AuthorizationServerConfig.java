7de08ac3598479bb8b223c5c24c1f6c2e9f3b880
mouxie <mouxie>
2019-06-03 14:35:43 +0800

    Change access token to JWT (JSON Web Token)

--- boot-oauth2-authorization/src/main/java/com/yanmouxie/config/AuthorizationServerConfig.java
+++ boot-oauth2-authorization/src/main/java/com/yanmouxie/config/AuthorizationServerConfig.java
@@ -1,14 +1,26 @@
 package com.yanmouxie.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 @EnableAuthorizationServer
@@ -19,11 +31,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	@Qualifier("authenticationManagerBean")
 	private AuthenticationManager authenticationManager;
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(jwtAccessTokenConverter());
+	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints)
 			throws Exception {
 		endpoints.authenticationManager(authenticationManager);
+		
+		endpoints.tokenStore(tokenStore()).tokenEnhancer(jwtAccessTokenConverter());
 	}
 
 	@Override
@@ -53,5 +72,25 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		oauthServer.tokenKeyAccess(
 				"isAnonymous() || hasAuthority('ROLE_TRUSTED_CLIENT')")
 				.checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");
+	}
+	
+	protected static class CustomTokenEnhancer extends JwtAccessTokenConverter {
+		@Override
+		public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+			Map<String, Object> additionalInfo = new HashMap<>();
+	        additionalInfo.put("organization", authentication.getName() + " nihao");
+	        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+			DefaultOAuth2AccessToken customAccessToken = new DefaultOAuth2AccessToken(accessToken);
+			return super.enhance(customAccessToken, authentication);
+		}
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+		JwtAccessTokenConverter converter = new CustomTokenEnhancer();
+		converter.setKeyPair(
+				new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"), "mypass".toCharArray()).getKeyPair("mytest"));
+		//converter.setSigningKey("123");
+		return converter;
 	}
 }
